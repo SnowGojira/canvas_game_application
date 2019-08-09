f6cb572f74c2a69f80db2ae836611590893f1978
SirSnow <hakuhal.snow@gmail.com>
2019-08-09 13:16:31 +0800

    add the random enemy function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//move the process of creating canvas here to fix the ctx 'undefined' problem
+//start the canvas
 (function initCanvas(){
     var doc = window.document,
         canvas = doc.createElement('canvas'),
@@ -7,20 +7,15 @@
     canvas.width = 505;
     canvas.height = 606;
     doc.body.appendChild(canvas);
-
+    //make the canvas and ctx to be global variables
     window.canvas = canvas;
     window.ctx = ctx;
 
 })();
 
-
-
-// Enemies class
+/////////////////////////////////////////Class/////////////////////////////////////
+//Enemy
 var Enemy = function(x,y,v) {
-    // Variables:
-    //image resource, start location, start velocity
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.start = x;
     this.x = x;
@@ -28,33 +23,26 @@ var Enemy = function(x,y,v) {
     this.v = v;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-//dt is used in engine.js, here it has no use.
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x = this.x + this.v;
-
+    //move with a velocity
+    this.x > canvas.width? this.x = this.start : this.x = this.x + this.v;
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    //console.log("enemy character: "+ Resources.get(this.sprite));
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
-    this.x > canvas.width? this.x = this.start : this.x = this.x;
-
-
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+//Gem
+var Gem = function(){};
+
+// Player
 var Player = function () {
     this.characterSprite = 'images/char-horn-girl.png';
     this.heartSprite = 'images/Heart.png';
+
+    this.count = 0;
+    this.heart = 3;
+
     this.startX = 202;
     this.startY = 405;
     this.x = this.startX;
@@ -62,24 +50,17 @@ var Player = function () {
     this.stepX = 0;
     this.stepY = 0;
     this.orient = 0;
-
-    this.count = 0;
-    this.heart = 3;
 };
 
 Player.prototype.update = function () {
-
     //move
-    //this.handleInput();
     if(this.orient != 0){
-
         this.x = this.x + this.stepX*this.orient;
         this.y = this.y + this.stepY*this.orient;
-
+        //empty the container for next move
         this.stepX = 0;
         this.stepY = 0;
     }
-
 };
 
 Player.prototype.render = function () {
@@ -90,7 +71,7 @@ Player.prototype.render = function () {
     for(let i = this.heart; i>=1 ; i--){
         ctx.drawImage(Resources.get(this.heartSprite), 500-i*35, 5,32,50);
     }
-    
+
     //render the counter
     drawCount(this.count);
 };
@@ -100,23 +81,18 @@ Player.prototype.handleInput = function (e) {
     switch (e) {
         case 'left' :
             this.x >= 101? this.stepX = 101 : this.stepX = 0;
-            //console.log(this.x >= 55.5? this.x - 55.5 : this.x);
-            //this.stepX = 101;
             this.orient = -1;
             break;
         case 'right' :
             this.x <= 303? this.stepX = 101 : this.stepX =0;
-            //this.stepX = 101;
             this.orient = 1;
             break;
         case 'up' :
             this.y >= 73? this.stepY = 83 : this.stepY = 0;
-            //this.stepY = 83;
             this.orient = -1;
             break;
         case 'down' :
             this.y <= 322? this.stepY = 83: this.stepY = 0;
-            //this.stepY = 83;
             this.orient = 1;
             break;
     }
@@ -128,6 +104,8 @@ Player.prototype.reset = function () {
 };
 
 
+
+///////////////////////////////////////////////Instantiate Objects///////////////////////////
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
@@ -138,11 +116,23 @@ var enemy3 = new Enemy(-60,145,5);
 var enemy4 = new Enemy(-200,228,2);
 
 var allEnemies = [enemy1,enemy2,enemy3,enemy4];
-//var allEnemies = [enemy1];
 
 var player = new Player();
 
 
+////////////////////////////////////////function to reference/////////////////////////////
+//random generator
+function RamdomEnemy(num){
+    let enemyArr = [];
+    let enemy;
+    let randomx,randomy,randomv;
+    //start X
+
+    let Rand = Math.random();
+    randomx = -1000 + Math.round((Rand * 10))/100;
+    console.log("随机数："+randomx);
+    return enemyArr;
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -151,16 +141,15 @@ document.addEventListener('keyup', function(e) {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+        13: 'enter'
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
 //collisions event logic
-var count = 0;
 var checkCollisions = function (){
-
     //collisions
     if(player.y == 239 ||
         player.y == 156 ||
@@ -195,7 +184,7 @@ var checkCollisions = function (){
 
 };
 
-//todo draw Text cannot be displayed. Because of in background.
+//Counter logic
 function drawCount(str) {
     ctx.fillStyle = "#000";
     ctx.font = "bold 20px Arial";
@@ -203,4 +192,3 @@ function drawCount(str) {
     ctx.textBaseline = "bottom";
     ctx.fillText(str,10,45);
 }
-//drawCount("start");
